71570ea8d609ecf200256282b16c64489d77839a
eightandseven <reinyear@gmail.com>
2018-04-04 15:00:01 +0800

    Added schedule script for automating schedule off

---
+++ prod/automate_sched.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set('Asia/Manila');
+class Database{
+    private $host = 'localhost';
+    private $user = 'root';
+    private $psswd = '';
+    private $dbname = 'powerboard';
+
+    public function connectDB(){
+        $mysql_string = "mysql:host=$this->host; dbname=$this->dbname";
+        $dbConnection = new PDO($mysql_string, $this->user, $this->psswd );
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    }
+}
+
+$conn = new Database();
+$conn = $conn->connectDB();
+while(true){
+    $date_time = date('Y-m-d H:i:s');
+
+    
+}
+
+
+
+
+?>
--- prod/automate_sched.py
+++ prod/automate_sched.py
@@ -4,14 +4,38 @@ from datetime import datetime as dt
 import time as tm
 
 #functions
-# #function to return date Yesterday string
+
+#function to return date Yesterday string
 def timeNow():
-    return dt.now().replace(microsecond=0)
+    return str(dt.now().replace(microsecond=0))
 
 #function to get mysql connection
 def dbConn():
     db = mysql.connect(host="localhost", user="root", passwd="", db="powerboard")
     return db
+
+#function to return a sched
+def schedNow(db, now):
+    cur = db.cursor()
+    sql = "SELECT id, socket_id, date_time_sched, action, description FROM schedule WHERE date_time_sched='"+now +"' AND state='READY'"
+    cur.execute(sql)
+    result = cur.fetchall()
+    count = cur.rowcount
+    cur.close()
+    return (count, result)
+
+#function to delete a sched
+def deleteSched(db, pid):
+    cur = db.cursor()
+    sql = "DELETE FROM schedule WHERE id=%s"
+    cur.execute(sql, (pid))
+    db.commit()
+    cur.close()
+
+#function to do something in rpi GPIO
+def doGPIOhere(row):
+    pid, socket_id, date_sched, action, description = row
+    print "Executed id", pid, "of socket", socket_id, "turn", action
 
 #START
 #set GPIO board as BCM
@@ -22,41 +46,31 @@ pinList = [18, 23, 24, 4, 17]
 # for i in pinList:
 # 	GPIO.setup(i, GPIO.OUT)
 
-# get database connection
-conn = dbConn()
+#check GPIO pin and put to dictionary with key matching socket number
+# pin_d = {str(pinList.index(x) + 1) : GPIO.input(x) for x in pinList}
+
 
 #infinite loop
 while True:
     try:
-        cur = conn.cursor()
+        # get database connection
+        conn = dbConn()
         now = timeNow()
-
-        #check GPIO pin and put to dictionary with key matching socket number
-        pin_d = {str(pinList.index(x) + 1) : x for x in pinList}
-        sql = "SELECT id, socket_id, date_time_sched, action, description FROM schedule WHERE date_time_sched=%s AND state=%s"
-        cur.execute(sql, (now, "READY"))
-        results = cur.fetchall()
-        count = cur.rowcount
-        print count
+        # get schedule for now
+        count, results = schedNow(conn, now)
         if (count >= 1):
             for row in results:
-                pid, socket_id, date_sched, action, description = row
-                print "Executed id", pid, "of socket", socket_id, "turn", action
-                
                 #do in GPIO here
-
-                curdel = conn.cursor()
-                sql2 = "DELETE FROM schedule WHERE id=%s"
-                curdel.execute(sql, (pid))
-                conn.commit()
-                curdel.close()
+                doGPIOhere(row)
+                pid = row[0]
+                #delete schedule task
+                deleteSched(conn, pid)
         else:
             print now
-        cur.close()
+        conn.close()
+
         tm.sleep(1)
     except Exception as e:
         print e
         tm.sleep(1)
-
-
 #END
