92d3cf65ead70a39a3750e5060c7aac8d30c95b7
eightandseven <reinyear@gmail.com>
2018-05-03 04:51:30 +0800

    added off

---
+++ prod/start_off.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+
+#START
+#set GPIO board as BCM
+GPIO.setmode(GPIO.BCM)
+pinList = [18, 23, 24, 4, 17] 
+
+# set GPIO pin as OUTPUT and turn off
+for i in pinList:
+	GPIO.setup(i, GPIO.OUT)
+    GPIO.input(i, GPIO.HIGH)
+#END
