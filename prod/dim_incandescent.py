b8b8f3cf497825b70fc2d8e178c1e597ebe6659f
eightandseven <reinyear@gmail.com>
2018-05-22 00:51:28 +0800

    added dimming for incandescent

---
+++ prod/dim_incandescent.py
@@ -0,0 +1,16 @@
+import sys
+import serial
+
+# # value from bash command
+dim = str(sys.argv[1])
+
+ser = serial.Serial(
+    
+    port='COM6',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+ser.write(dim)
